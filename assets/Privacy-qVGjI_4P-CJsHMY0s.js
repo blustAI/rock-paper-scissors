7888126adc0fab079b36361310a37a9b919c3a8c
Eugene <eugene@blust.ai>
2024-04-06 12:54:26 -0700

    Updates

---
+++ assets/Privacy-qVGjI_4P-CJsHMY0s.js
@@ -0,0 +1,48 @@
+import{u as e}from"./index-DiUrmAau.js";const t=()=>e.jsx("div",{style:{whiteSpace:"pre-line",padding:"1em"},dangerouslySetInnerHTML:{__html:`
+    <b>Privacy Policy</b>
+
+    1. Introduction
+    
+    Welcome to Blust.AI! Your privacy is of paramount importance to us. This Privacy Policy outlines how we collect, use, and safeguard your personal information. By using our services, you agree to the terms outlined here.
+    
+    2. Information We Collect
+    
+    Personal Information: Such as name, email address, payment details, and any other information you provide to us.
+    Usage Information: Details of your use of our services, including traffic data, location data, and other communication data.
+    Device Information: Information about the device you use to access our services, such as IP address, browser type, and version.
+    
+    3. How We Use Your Information
+    
+    To provide, maintain, and improve our services.
+    To process transactions and send related information.
+    To respond to your comments, questions, and requests.
+    To send technical notices, updates, and support messages.
+
+    4. Information Sharing and Disclosure
+    
+    We do not sell, rent, or share your information with third parties outside Blust.AI without your consent, except to provide products or services you’ve requested.
+    
+    5. Cookies and Other Tracking Technologies
+    
+    We use cookies and similar technologies to enhance user experience, track user activities, and for other purposes as described in this policy.
+    
+    6. Data Security
+    
+    We implement reasonable security practices and procedures to protect the information that we collect, but we cannot guarantee that such information may not be accessed, disclosed, altered, or destroyed by breach.
+    
+    7. User’s Rights
+    
+    You have the right to access, update, or delete your personal information, and we will take reasonable steps to allow you to correct, amend, delete, or limit the use of your personal data.
+    
+    8. Children’s Privacy
+    
+    Our service is not directed to persons under 18. We do not knowingly collect or solicit personal information from anyone under the age of 18.
+    
+    9. Changes to the Privacy Policy
+    
+    We may update this Privacy Policy periodically. We will notify you of any changes by posting the new Privacy Policy on this page.
+    
+    10. Contact Us
+    
+    For any queries regarding the privacy policy, please contact us at info@blust.ai.
+    `}});export{t as default};
---
+++ assets/Terms-TOn6S9KA-DZ1HG5CJ.js
@@ -0,0 +1,80 @@
+import{u as e}from"./index-DiUrmAau.js";const t=()=>e.jsx("div",{style:{whiteSpace:"pre-line",padding:"1em"},dangerouslySetInnerHTML:{__html:`
+    <b>User Agreement</b>
+
+    1. Introduction
+
+    Welcome to Blust.AI! This User Agreement outlines the rules and regulations for using our platform. Please read them carefully before utilizing our services.
+    
+    2. Definitions
+    
+    Services: All functionalities, software, and activities related to Blust.AI.
+    Users: All individuals who access and/or use Blust.AI.
+
+    3. User Registration and Account Responsibility
+    
+    Users must provide accurate registration details and safeguard account credentials. Users are accountable for all activities occurring under their account.
+    
+    4. Privacy and Data Protection
+    
+    We value your privacy and strive to safeguard your personal data. For more details, refer to our Privacy Policy.
+    
+    5. User Obligations
+    
+    Users agree to adhere to all applicable laws and regulations, respect other users, and maintain the integrity of the platform.
+    
+    6. Rights and Restrictions
+    
+    Blust.AI grants you a limited, non-exclusive license to use our services, subject to this agreement. Unauthorized reproduction, distribution, or exploitation of the services is prohibited.
+    
+    7. Payment and Subscription
+    
+    Blust.AI offers different plans for using our AI tools and services, each with its unique pricing and features:
+    <i>Explorer Plan</i>
+    o	Price: $0/mo
+    o	500 monthly credits, excludes premium tools
+
+    <i>Innovator Plan</i>
+    o	Price: $9.99/mo
+    o	9,000 monthly credits
+
+    <i>Creator Plan</i>
+    o	Price: $19.99/mo
+    o	20,000 monthly credits
+    
+    <i>Enterprise Plan</i>
+    o	Customizable credit package
+
+    By selecting a plan, you agree to pay Blust.AI the monthly or annual subscription fees indicated for that service. Payments will be charged on the day you sign up for a service and will cover the use of that service for a monthly or annual period as indicated.
+    
+    
+    8. Limitation of Liability and Disclaimer of Warranties
+    
+    Blust.AI, being a frontend for third-party AI models, does not guarantee the accuracy, reliability, or correctness of AI-generated answers and is not responsible for any consequences arising from utilizing them. We provide the services "as is" and disclaim all warranties to the fullest extent permissible by law.
+    
+    9. Indemnification
+    
+    Users agree to indemnify and hold Blust.AI harmless from any claims, losses, or damages arising from or related to breach of this agreement or misuse of our services.
+    
+    10. Termination
+    
+    Blust.AI reserves the right to suspend or terminate accounts and access to services for violations of this agreement.
+    
+    11. Governing Law
+    
+    This agreement shall be governed by the laws of State of California.
+    
+    12. Contact Information
+    
+    For inquiries, please contact info@blust.ai.
+    
+    13. Changes to the Agreement
+    
+    Blust.AI may modify this agreement at any time. Users will be notified of any changes, and continued use of the services implies acceptance.
+    
+    14. Miscellaneous
+    
+    Blust.AI is a front-end interface and is not responsible for the AI models and generated content, which are provided by third parties.
+    
+    Conclusion
+    By using our services, you agree to abide by this User Agreement. Please ensure to review it periodically for any updates.
+    `}});export{t as default};
