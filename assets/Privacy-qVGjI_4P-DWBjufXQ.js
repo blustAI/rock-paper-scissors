186b4aae957f56f9df1f2f74b80f0c1dee4ca10f
Eugene <eugene@blust.ai>
2024-04-19 14:50:11 -0700

    Updates

--- assets/Privacy-qVGjI_4P-DWBjufXQ.js
+++ assets/Privacy-qVGjI_4P-DWBjufXQ.js
@@ -1,4 +1,4 @@
-import{u as e}from"./index-CFatjN0z.js";const t=()=>e.jsx("div",{style:{whiteSpace:"pre-line",padding:"1em"},dangerouslySetInnerHTML:{__html:`
+import{u as e}from"./index-JQIzhxEJ.js";const t=()=>e.jsx("div",{style:{whiteSpace:"pre-line",padding:"1em"},dangerouslySetInnerHTML:{__html:`
     <b>Privacy Policy</b>
 
     1. Introduction
